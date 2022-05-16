f612ef4c1fd6926e4f21931d3294d906cbd04ec6
CEIBA\pablo.tabares <pablo.tabares@ceiba.com.co>
2022-05-15 20:34:52 -0500

    Se agrega clase para descomponer fechas y usar partes de la fecha para realizar validaciones en el dominio..

---
+++ comun/comun-dominio/src/main/java/com/ceiba/dominio/DescomponerFecha.java
@@ -0,0 +1,29 @@
+package com.ceiba.dominio;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalField;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DescomponerFecha {
+
+    private DescomponerFecha() {}
+
+    public static Integer porDia(Date fechaADescomponer) {
+        return convertirDateALocalDate(fechaADescomponer).getMonthValue();
+    }
+
+    public static LocalDate convertirDateALocalDate(Date fechaAConvertir) {
+        return LocalDate.ofInstant(fechaAConvertir.toInstant(), ZoneId.systemDefault());
+    }
+
+    public static Integer porDiaDeSemana(Date fechaADescomponer) {
+        return convertirDateALocalDate(fechaADescomponer).getDayOfWeek().getValue();
+    }
+}
