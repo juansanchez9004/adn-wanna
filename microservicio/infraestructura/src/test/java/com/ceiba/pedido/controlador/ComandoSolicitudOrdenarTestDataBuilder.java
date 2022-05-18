2af5a0d0495aeae967f3f8c50645463ffa17e520
CEIBA\pablo.tabares <pablo.tabares@ceiba.com.co>
2022-05-18 08:07:17 -0500

    Se modifica test debido a error en el resultado.

--- microservicio/infraestructura/src/test/java/com/ceiba/pedido/controlador/ComandoSolicitudOrdenarTestDataBuilder.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/pedido/controlador/ComandoSolicitudOrdenarTestDataBuilder.java
@@ -3,6 +3,9 @@ package com.ceiba.pedido.controlador;
 import com.ceiba.comando.ComandoProductoOrdenar;
 import com.ceiba.comando.ComandoPuntoEntrega;
 import com.ceiba.comando.ComandoSolicitudOrdenar;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -23,6 +26,8 @@ public class ComandoSolicitudOrdenarTestDataBuilder {
 
     public ComandoSolicitudOrdenarTestDataBuilder crearPorDefecto() {
         this.idCliente = 1l;
+        this.fecha = Date.from(LocalDate.of(2022, 5, 17)
+                .atStartOfDay(ZoneId.systemDefault()).toInstant());
         this.comandoPuntoEntrega = new ComandoPuntoEntrega("Calle 5c - 2 sur # 345", "Medellin");
         this.comandoProductosOrdenados.add(new ComandoProductoOrdenar(1l, 3));
         this.comandoProductosOrdenados.add(new ComandoProductoOrdenar(2l, 2));
