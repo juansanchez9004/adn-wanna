678a241db953af731f9f906586ed5f66d54a89a5
CEIBA\pablo.tabares <pablo.tabares@ceiba.com.co>
2022-05-23 09:20:15 -0500

    Se agrega test de integración para el nuevo endpoint de pedidos pendientes.

--- microservicio/infraestructura/src/test/java/com/ceiba/pedido/controlador/ConsultaControladorPedidoTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/pedido/controlador/ConsultaControladorPedidoTest.java
@@ -39,4 +39,17 @@ class ConsultaControladorPedidoTest {
                 .andExpect(jsonPath("$[0].sitioEntrega", is("Calle 90 c # 584 - Envigado")))
                 .andExpect(jsonPath("$[0].estado", is("ENTREGADO")));
     }
+
+    @Test
+    void consultarPedidosPendientes() throws Exception {
+
+        mocMvc.perform(get("/pedido/pendientes")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].valorTotal", is(1268960.0)))
+                .andExpect(jsonPath("$[0].fecha", is("2022-05-16")))
+                .andExpect(jsonPath("$[0].sitioEntrega", is("Calle 35 # 42 - 46 - Itagui")))
+                .andExpect(jsonPath("$[0].estado", is("PENDIENTE")));
+    }
 }
