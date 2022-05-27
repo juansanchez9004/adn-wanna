7982a46f8094ef4aebe3969e4c76e4127ef40e4d
CEIBA\pablo.tabares <pablo.tabares@ceiba.com.co>
2022-05-26 23:39:07 -0500

    Se modifica DTO de resumen pedido para cambiar los Date por LocalDate.

--- microservicio/dominio/src/main/java/com/ceiba/pedido/modelo/dto/ResumenPedidoDTO.java
+++ microservicio/dominio/src/main/java/com/ceiba/pedido/modelo/dto/ResumenPedidoDTO.java
@@ -4,13 +4,14 @@ import com.ceiba.pedido.modelo.entidad.EstadoPedido;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Date;
 
 @AllArgsConstructor
 @Getter
 public class ResumenPedidoDTO {
     private Long id;
-    private Date fecha;
+    private LocalDate fecha;
     private String sitioEntrega;
     private BigDecimal valorTotal;
     private EstadoPedido estado;
